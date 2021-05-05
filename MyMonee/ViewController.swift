2962da3a7dc3b183c0ad0b2113a18d6e8021c199
M. Kamil <mk.kamiil@gmail.com>
2021-05-05 10:57:31 +0700

    Initial Commit

---
+++ MyMonee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMonee
+//
+//  Created by MacBook on 05/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
